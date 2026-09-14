d8dad4adede48249b670cca09de119dcedb7772d
root <root@ip-172-31-15-83.ap-south-1.compute.internal>
2026-09-14 04:30:23 +0000

    tag is changed

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "app" {
   instance_type = var.instance_type
 
   tags = {
-  Name        = "jenkins-terraform-server"
+  Name        = "jenkins-terraform-server_for the change"
   Environment = "dev"
 }
 }
